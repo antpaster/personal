6aef690a58cd7b0dbc228fb8c82b1ea45ff193b5
AVP <antpaster@gmail.com>
2017-09-07 14:39:47 +0300

    Koep35 All data is entered. Algorithm works with static payload on the hanging points.

--- Koep35/main.c
+++ Koep35/main.c
@@ -6,20 +6,13 @@
 
 int main( void)
 {
-    double y = 40.0 * GrToRad, z = 13.0 * GrToRad;
+    double y = -140.0 * GrToRad, z = 13.0 * GrToRad;
     uint8_t limY = 0, limZ = 0, shadow1deg = 0, shadow5deg = 0;
 
-    ThangPointShadow iHps;
-    for( iHps = PLANE_SHAPE_Y_POS; iHps < HANG_POINT_SHADOW_ALL; ++iHps)
-    {
-        y = 40.0 * GrToRad;
-        z = 13.0 * GrToRad;
-
-        koep35checkVisionAngles( iHps, &y, &z, &limY, &limZ, &shadow1deg, &shadow5deg);
-        printf( "\n\n%u:\ny = %f, z = %f limY = %i, limZ = %i, shadow1deg = %i,"
-            " shadow5deg = %i\n", iHps, y * RadToGr, z * RadToGr, limY, limZ, shadow1deg,
-            shadow5deg);
-    }
+    koep35checkVisionAngles( &y, &z, &limY, &limZ, &shadow1deg, &shadow5deg);
+    printf( "\ny = %f, z = %f limY = %i, limZ = %i, shadow1deg = %i,"
+        " shadow5deg = %i\n", y * RadToGr, z * RadToGr, limY, limZ, shadow1deg,
+        shadow5deg);
 
     getch( );
     return 0;
