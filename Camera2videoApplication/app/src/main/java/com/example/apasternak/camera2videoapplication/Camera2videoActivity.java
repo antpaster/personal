7c179d36cf9d6b4eef13eb9577e38070617e888c
antpaster <antpaster@gmail.com>
2017-10-10 17:12:45 +0300

    Branch: camera2\n Project: Camera2videoApplication\n New icons added, camera created and the background process for her. The orientation considering is under development.

--- Camera2videoApplication/app/src/main/java/com/example/apasternak/camera2videoapplication/Camera2videoActivity.java
+++ Camera2videoApplication/app/src/main/java/com/example/apasternak/camera2videoapplication/Camera2videoActivity.java
@@ -1,21 +1,35 @@
 package com.example.apasternak.camera2videoapplication;
 
+import android.content.Context;
 import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseIntArray;
+import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Toast;
 
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
 public class Camera2videoActivity extends AppCompatActivity {
 
+    /// Texture view for background
     private TextureView mTextureView;
     private TextureView.SurfaceTextureListener mSurfaceTextureListener
         = new TextureView.SurfaceTextureListener() {
         @Override
         public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-            Toast.makeText(getApplicationContext(), "TextureView is available",
-                Toast.LENGTH_SHORT).show();
+            setupCamera(width, height);
         }
 
         @Override
@@ -34,6 +48,43 @@ public class Camera2videoActivity extends AppCompatActivity {
         }
     };
 
+    /// Camera device definition
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback
+        = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            camera.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            camera.close();
+            mCameraDevice = null;
+        }
+    };
+
+    /// Background thread and its handler definition
+    private HandlerThread mBackgroundHandlerThread;
+    private Handler mBackgroundHandler;
+
+    private String mCameraId;
+
+    /// Orientations array
+    private static SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,11 +97,22 @@ public class Camera2videoActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        if (mTextureView.isAvailable()) {
+        startBackgroundThread();
 
+        if (mTextureView.isAvailable()) {
+            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
         } else {
             mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        closeCamera();
+
+        stopBackgroundThread();
+
+        super.onPause();
     }
 
     @Override
@@ -64,5 +126,69 @@ public class Camera2videoActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
+    }
+
+    private void setupCamera(int width, int height) {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (String cameraId : cameraManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics
+                    = cameraManager.getCameraCharacteristics(cameraId);
+
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING)
+                    == CameraCharacteristics.LENS_FACING_FRONT) {
+                    continue;
+                }
+
+                int deviceOrientation = getWindowManager().getDefaultDisplay().getRotation();
+                int totalRotation = sensorToDeviceRotation(cameraCharacteristics,
+                    deviceOrientation);
+                boolean swapRotation = (totalRotation == 90 || totalRotation == 270);
+
+                int rotatedWidth = width;
+                int rotatedHeight = height;
+                if (swapRotation) {
+                    rotatedWidth = height;
+                    rotatedHeight = width;
+                }
+
+                mCameraId = cameraId;
+                return;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    private void startBackgroundThread() {
+        mBackgroundHandlerThread = new HandlerThread("Camera2videoImage");
+        mBackgroundHandlerThread.start();
+        mBackgroundHandler = new Handler(mBackgroundHandlerThread.getLooper());
+    }
+
+    private void stopBackgroundThread() {
+        mBackgroundHandlerThread.quitSafely();
+        try {
+            mBackgroundHandlerThread.join();
+            mBackgroundHandlerThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics,
+        int deviceOrientation) {
+        int sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        deviceOrientation = ORIENTATIONS.get(deviceOrientation);
+
+        return (sensorOrientation + deviceOrientation + 360) % 360;
     }
 }
