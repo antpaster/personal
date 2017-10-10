080247c7cf31688a5e35c5e4489e4583a48394b5
antpaster <antpaster@gmail.com>
2017-10-10 14:43:53 +0300

    Branch: master Project: Camera2videoApplication Commit for remote pushing.

--- Camera2videoApplication/app/src/main/java/com/example/apasternak/camera2videoapplication/Camera2videoActivity.java
+++ Camera2videoApplication/app/src/main/java/com/example/apasternak/camera2videoapplication/Camera2videoActivity.java
@@ -1,13 +1,68 @@
 package com.example.apasternak.camera2videoapplication;
 
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Toast;
 
 public class Camera2videoActivity extends AppCompatActivity {
+
+    private TextureView mTextureView;
+    private TextureView.SurfaceTextureListener mSurfaceTextureListener
+        = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            Toast.makeText(getApplicationContext(), "TextureView is available",
+                Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera2video);
+
+        mTextureView = (TextureView) findViewById(R.id.textureView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mTextureView.isAvailable()) {
+
+        } else {
+            mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        View decorView = getWindow().getDecorView();
+
+        if (hasFocus) {
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        }
     }
 }
