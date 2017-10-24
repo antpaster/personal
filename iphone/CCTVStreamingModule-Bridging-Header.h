1e2c387fba2c5621725e337ec98ea474dac21b35
Александр <ps@MacBook-Pro-Aleksandr.local>
2017-10-24 16:15:23 +0300

    Добавлены хедеры

--- iphone/CCTVStreamingModule-Bridging-Header.h
+++ iphone/CCTVStreamingModule-Bridging-Header.h
@@ -2,3 +2,12 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "RTPPacketizer.h"
+#import "SimpleConnection.h"
+#import "VideoConnection.h"
+#import "InterleavedPacketizer.h"
+#import "AACEncoder.h"
+#import "GeoLocation.h"
+#import "GeoLocationDelegate.h"
+#import "GeoLocationServer.h"
+#import "GpsServerConnection.h"
