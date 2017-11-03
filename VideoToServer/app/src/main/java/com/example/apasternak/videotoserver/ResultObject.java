06fa94587dc398abb4b9a507d09f1743aa9e705e
antpaster <antpaster@gmail.com>
2017-11-03 11:00:00 +0300

    Branch: video. All video related projects will dwell here.

---
+++ VideoToServer/app/src/main/java/com/example/apasternak/videotoserver/ResultObject.java
@@ -0,0 +1,15 @@
+package com.example.apasternak.videotoserver;
+
+/**
+ * Created by apasternak on 25.10.17.
+ */
+
+public class ResultObject {
+    private String success;
+    public ResultObject(String success) {
+        this.success = success;
+    }
+    public String getSuccess() {
+        return success;
+    }
+}
